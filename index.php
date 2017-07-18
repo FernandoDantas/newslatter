b8e890c02e8793bcf4b67dbce7fd3b9ff032b9f9
Fernando Dantas <fernandojrsud@hotmail.com>
2017-07-18 16:11:23 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require 'config.php'?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Envio de emails</title>
+	<link rel="stylesheet" type="text/css" href="bower_components/semantic/dist/semantic.css">
+</head>
+<body>
+
+<div class="ui container">
+
+
+</div>
+
+
+<script src="bower_components/jquery/dist/jquery.js" type="text/javascript" charset="utf-8" async defer></script>
+</body>
+</html>
